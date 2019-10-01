1b70659c7c195a6712d026c551da7c8ff047ae24
Polar Bear <bballare21@gmail.com>
2019-10-01 13:32:10 -0700

    modified topalbums html and js file

---
+++ topalbums.js
@@ -0,0 +1,55 @@
+
+let trackSearch = "lover"
+let trackArtist = ""
+let artist = "anberlin";
+
+
+function getAlbum() {
+    let lastAPIKey = "8173e09c55bc9e07b713c6edcd74e63e";
+    let lastTopAlbum = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&api_key=" + lastAPIKey + "&format=json";
+
+$.ajax({
+    url: lastTopAlbum,
+    method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        let top1 = response.topalbums.album[0].name;
+        let top2 = response.topalbums.album[1].name;
+        let top3 = response.topalbums.album[2].name;
+        let top4 = response.topalbums.album[3].name;
+        console.log(top1);
+        console.log(top2);
+
+        let data = response.topalbums.album
+        let alDiv1 = $('<div>');
+        let alDiv2 = $('<div>');
+        let alDiv3 = $('<div>');
+        let alDiv4 = $('<div>');
+        let p1 = $('<p>').text(top1);
+        let p2 = $('<p>').text(top2);
+        let p3 = $('<p>').text(top3);
+        let p4 = $('<p>').text(top4);
+        let image1 = $('<img>');
+        let image2 = $('<img>');
+        let image3 = $('<img>');
+        let image4 = $('<img>');
+        image1.attr('src', data[0].image[2]['#text']);
+        console.log(image1);
+        image2.attr('src', data[1].image[2]['#text']);
+        image3.attr('src', data[2].image[2]['#text']);
+        image4.attr('src', data[3].image[2]['#text']);
+        alDiv1.append(p1);
+        alDiv1.prepend(image1);
+        alDiv2.append(p2);
+        alDiv2.prepend(image2);
+        alDiv3.append(p3);
+        alDiv3.prepend(image3);
+        alDiv4.append(p4);
+        alDiv4.prepend(image4);
+        $('#album1').append(alDiv1);
+        $('#album2').append(alDiv2);
+        $('#album3').append(alDiv3);
+        $('#album4').append(alDiv4);
+    });
+}
+getAlbum();
